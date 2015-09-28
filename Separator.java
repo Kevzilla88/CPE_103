bc30fad47881235b2ff7d2401dd7f36e221c8608
GeoffWacker <geoffwacker@gmail.com>
2015-09-28 11:09:22 -0700

    Lab 02 First commit

---
+++ Separator.java
@@ -0,0 +1,16 @@
+/**
+ * Lab 02 - Separator
+ * @author geoffwacker, kevinliu
+ * 09.28.15
+ */
+
+import java.util.*;
+
+public class Separator 
+{
+	public static void main(String [ ] args)
+	{
+		
+	}
+
+}
