168bea01fb63e98ec386373292874580afcebe33
GeoffWacker <geoffwacker@gmail.com>
2015-10-05 10:39:21 -0700

    Fleshing out project 1
    Earlier commit with completed Lab3 didn’t work for some reason, I’ll
    try again when I get home. In the meantime though, I started fleshing
    out Project1.

--- Project1/Converter.java
+++ Project1/Converter.java
@@ -8,8 +8,25 @@ import java.util.*;
 
 public class Converter 
 {
-	public static void main(String [ ] args)
+	/**
+	 * Convert arithmetic expression in infix notation to postfix notation.
+	 * @param expression the given arithmetic expression in infix notation.
+	 * @return postfix the postfix expression.
+	 */
+	public static String infixToPostfix (String expression)
 	{
-
+		//MUST use algorithm found in textbook. 
+		return "Placeholder line";
+		
+	}
+	
+	/**
+	 * Calculate the value of the postfix expression.
+	 * @param expression the postfix expression to be calculated.
+	 * @return the value (double) of the postfix expression.
+	 */
+	public static double postfixValue (String expression)
+	{
+		return 0.0;
 	}
 }
--- Project1/MyStack.java
+++ Project1/MyStack.java
@@ -1,6 +1,7 @@
 /**
  * Project 01 - MyStack<T>
  * @author geoffwacker, kevinliu
+ * @id gwacker, 
  * 10.04.15
  */
 
@@ -8,8 +9,42 @@ import java.util.*;
 
 public class MyStack<T> 
 {
-	public static void main(String [ ] args)
+	//Instance variables.
+	private Node first;
+	
+	//Nested node class.
+	private class Node
 	{
-
+		public T item;
+		public Node next;
+	}
+	
+	//Constructor for an empty stack.
+	public MyStack()
+	{
+		first = null;
+	}
+	//Parameter object is added to the top of the stack.
+	public void push(T element)
+	{
+		
+	}
+	
+	//Removes and returns the element at the top of the stack.
+	public T pop()
+	{
+		//If stack is empty, throw EmptyStackException.
+		
+		//Placeholder code.
+		return null;
+	}
+	
+	//Return the top value of the stack without removing it.
+	public T peek()
+	{
+		//If stack is empty, throw EmptyStackException.	
+		
+		//Placeholder code.
+		return null;
 	}
 }
