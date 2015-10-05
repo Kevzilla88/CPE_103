2e8ca908034d490d5e366f67b8240254f14071cd
GeoffWacker <geoffwacker@gmail.com>
2015-10-04 18:25:39 -0700

    Completed Lab3
    Code now passes both tests.

--- Lab3/LQueue.java
+++ Lab3/LQueue.java
@@ -6,10 +6,106 @@
 
 import java.util.*;
 
-public class LQueue<T> 
+public class LQueue<T>
 {
-	public static void main(String [ ] args)
+	//Instance variables.
+	private Node front, end;
+	
+	//Initializes an empty queue.
+	public LQueue()
 	{
-
+		front = null;
+		end = null;
+	}
+	
+	//Node.
+	private class Node
+	{
+		public T item;
+		public Node next;
+	}
+	
+	//Nested exception class with two constructors.
+	public static class MyException extends RuntimeException
+	{
+		public MyException()
+		{
+			super();
+		}
+		
+		public MyException(String message)
+		{
+			super(message);
+		}	
+	}
+	
+	/**
+	 * Insert a new item into the queue.
+	 * @param item the item to insert.
+	 */
+	public void enqueue(T item)
+	{
+		Node oldEnd = end;
+		end = new Node();
+		end.item = item;
+		end.next = null;
+		if (isEmpty())
+		{
+			front = end;
+		}
+		else
+		{
+			oldEnd.next = end;
+		}
+	}
+	
+	/**
+	 * Remove and return the last added item.
+	 * @return the last added item.
+	 * @throws MyException if the queue is empty.
+	 */
+	public T dequeue()
+	{
+		//If the queue is empty, throw an exception.
+		try
+		{
+			if (isEmpty())
+			{
+				throw new MyException();
+			}
+			
+		}
+		//Catch the exception and print a message.
+		catch (MyException e)
+		{
+			System.out.println("Invalid operation: the queue is empty.");
+		}
+		
+		//The queue isn't empty, so carry on.
+		if (!isEmpty())
+		{
+			T item = front.item;
+			front = front.next;
+			
+			/**
+			if (isEmpty())
+			{
+				end = null;
+			}
+			**/
+			return item;
+		}
+		
+		//Return null to handle the exception case.
+		return null;
+	}
+	
+	/**
+	 * Test if the queue is logically empty.
+	 * @return true if empty, false otherwise.
+	 */
+	public boolean isEmpty()
+	{
+		return front == null;
 	}
 }
--- Lab3/LQueueDriver.java
+++ Lab3/LQueueDriver.java
@@ -6,10 +6,93 @@
 
 import java.util.*;
 
-public class LQueueDriver 
+public class LQueueDriver extends LQueue
 {
 	public static void main(String [ ] args)
 	{
-
+		//Variable declaration.
+		char input = 'a';
+		
+		//Create an empty integer queue.
+		LQueue<Integer> intQueue = new LQueue<Integer>();
+		
+		//Create a scanner.
+		Scanner in = new Scanner(System.in);
+		
+		//Loop to ask for input.
+		while (input != 'q')
+		{
+			System.out.println("Choose one of the following operations: ");
+			System.out.println("- enqueue/add (enter the letter a) ");
+			System.out.println("- dequeue/delete (Enter the letter d) ");
+			System.out.println("- check if the list is empty (enter the letter e) ");
+			System.out.println("- quit (enter the letter q) ");
+			input = in.nextLine().charAt(0);
+			
+			//Switch statement to handle input.
+			switch (input)
+			{
+				//User wants to add.
+				case 'a':
+				{
+					System.out.println("What would you like to add?");
+					try
+					{
+						int enqueueInput = in.nextInt();
+						intQueue.enqueue(enqueueInput);
+						System.out.println(enqueueInput + " enqueued");
+						in.nextLine();
+						break;
+					}
+					catch (InputMismatchException i)
+					{
+						System.out.println("Invalid enqueue input!");
+						in.nextLine();
+						break;
+					}
+				}
+				
+				//User wants to remove.
+				case 'd':
+				{
+					Integer intQueueOutput = intQueue.dequeue();
+					if (intQueueOutput == null)
+					{
+						break;
+					}
+					else
+					{
+						System.out.println(intQueueOutput + " dequeued");
+						break;
+					}	
+				}
+				
+				//User wants to see if queue is empty.
+				case 'e':
+				{
+					System.out.println(intQueue.isEmpty());
+					break;
+				}
+				
+				//User wants to quit.
+				case 'q':
+				{
+					in.close();
+					//Empty the queue, one value at a time, separated by spaces.
+					while (!intQueue.isEmpty())
+					{
+						System.out.print(intQueue.dequeue() + " ");
+					}
+					break;
+				}
+				
+				//User entered invalid input.
+				default:
+				{
+					System.out.println("Invalid menu input!");
+					break;
+				}	
+			}	
+		}	
 	}
 }
