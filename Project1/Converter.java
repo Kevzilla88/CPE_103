ba7218ed7f575ba441809b4d75382ec318319a9d
Kevzilla88 <Kevinliu2336@gmail.com>
2015-10-09 11:11:13 -0700

    Doesn't work yet.

--- Project1/Converter.java
+++ Project1/Converter.java
@@ -3,7 +3,7 @@
  * @author geoffwacker, kevinliu
  * 10.04.15
  */
-
+package com.company;
 import java.util.*;
 
 public class Converter 
@@ -15,18 +15,112 @@ public class Converter
 	 */
 	public static String infixToPostfix (String expression)
 	{
-		//MUST use algorithm found in textbook. 
-		return "Placeholder line";
+		//Creates an empty string to hold output
+		String postfix = "";
+
+		//Creates a new stack to hold inputs
+		MyStack converter = new MyStack();
+
+		//Creates a scanner to process the input expression
+		Scanner in = new Scanner(expression);
+		while (in.hasNext()){
+			String token = in.next();
+
+			if (token.equals("(")){
+				converter.push("(");
+				}
+			else if(token.equals(")")){
+				while (!token.equals("(")){
+					postfix= postfix+ converter.pop() + " ";
+				}
+			}
+			else if( token.equals("^")){
+				while(!(token.equals("^")||token.equals("*")||token.equals("/")||token.equals("+")||token.equals("-"))){
+					postfix= postfix+ converter.pop() + " ";
+				}
+				converter.push("^");
+			}
+			else if( token.equals("*")) {
+				while (!(token.equals("*") || token.equals("/") || token.equals("+") || token.equals("-"))) {
+					postfix= postfix+ converter.pop() + " ";
+				}
+				converter.push("*");
+			}
+			else if(token.equals("/")) {
+				while (!(token.equals("*") || token.equals("/") || token.equals("+") || token.equals("-"))) {
+					postfix= postfix+ converter.pop() + " ";
+				}
+				converter.push("/");
+			}
+			else if(token.equals("+")) {
+				while (!( token.equals("+") || token.equals("-"))) {
+					postfix= postfix+ converter.pop() + " ";
+				}
+				converter.push("+");
+			}
+			else if(token.equals("-")) {
+				while (token.equals("+") || token.equals("-")) {
+					postfix= postfix + converter.pop() + " ";
+				}
+				converter.push("/");
+			}
+			else{
+				converter.push(token);
+			}
+
+		}
+		while(!converter.isEmpty()){
+			postfix= postfix+ converter.pop() + " ";
+		}
+		return postfix;
 		
 	}
 	
 	/**
 	 * Calculate the value of the postfix expression.
 	 * @param expression the postfix expression to be calculated.
-	 * @return the value (double) of the postfix expression.
+	 * @return the value Double.parseDouble((String)of the postfix e)xpression.
 	 */
 	public static double postfixValue (String expression)
 	{
-		return 0.0;
+		double lhs, rhs, answer;
+		//Creates a new stack to hold inputs
+		MyStack calculator = new MyStack();
+
+		//Creates a scanner to process the input expression
+		Scanner in = new Scanner(expression);
+		//
+		while (in.hasNext()){
+			String token = in.next();
+			if(token.matches("[0-9]")){
+				calculator.push(token);
+			}
+			else if (token.equals("^")){
+				rhs= Double.parseDouble((String)calculator.pop());
+				lhs = Double.parseDouble((String)calculator.pop());
+				calculator.push(Math.pow(lhs,rhs));
+			}
+			else if (token.equals("*")){
+				rhs= Double.parseDouble((String)calculator.pop());
+				lhs = Double.parseDouble((String)calculator.pop());
+				calculator.push(lhs*rhs);
+			}
+			else if (token.equals("/")){
+				rhs= Double.parseDouble((String)calculator.pop());
+				lhs = Double.parseDouble((String)calculator.pop());
+				calculator.push(lhs/rhs);
+			}
+			else if (token.equals("+")){
+				rhs= Double.parseDouble((String)calculator.pop());
+				lhs = Double.parseDouble((String)calculator.pop());
+				calculator.push(lhs+rhs);
+			}
+			else if (token.equals("-")){
+				rhs= Double.parseDouble((String)calculator.pop());
+				lhs = Double.parseDouble((String)calculator.pop());
+				calculator.push(lhs-rhs);
+			}
+		}
+		return(Double.parseDouble((String)calculator.pop()));
 	}
 }
