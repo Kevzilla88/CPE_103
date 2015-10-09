82a9e18c03efa9655a40e17013e4dc9d6180f052
Kevzilla88 <Kevinliu2336@gmail.com>
2015-10-09 12:38:38 -0700

    Actually working now

--- Project1/Converter.java
+++ Project1/Converter.java
@@ -6,115 +6,107 @@
 package com.company;
 import java.util.*;
 
-public class Converter 
-{
-	/**
-	 * Convert arithmetic expression in infix notation to postfix notation.
-	 * @param expression the given arithmetic expression in infix notation.
-	 * @return postfix the postfix expression.
-	 */
-	public static String infixToPostfix (String expression)
-	{
-		//Creates an empty string to hold output
-		String peekNext;
-		String postfix = new String();
+public class Converter {
+   /**
+    * Convert arithmetic expression in infix notation to postfix notation.
+    *
+    * @param expression the given arithmetic expression in infix notation.
+    * @return postfix the postfix expression.
+    */
+   public static String infixToPostfix(String expression) {
+      //Creates an empty string to hold output
+      String peekNext;
+      String postfix = new String();
 
-		//Creates a new stack to hold inputs
-		MyStack converter = new MyStack();
+      //Creates a new stack to hold inputs
+      MyStack converter = new MyStack();
 
-		//Creates a scanner to process the input expression
-		Scanner in = new Scanner(expression);
-		
-		while (in.hasNext()){
-			String token = in.next();
+      //Creates a scanner to process the input expression
+      Scanner in = new Scanner(expression);
 
-			if (token.equals("(")){
-				converter.push(token);
-				}
-			else if(token.equals(")")){
-				peekNext = (String) converter.peek();
-				while ( !peekNext.equals("("))
-				{
-					postfix+= converter.pop() + " ";
-					peekNext = (String) converter.peek();
-				}
-				converter.pop();
-			}
-			else if( token.equals("*")) {
-				converter.push("*");
-			}
-			else if(token.equals("/")) {
-				converter.push("/");
-			}
-			else if(token.equals("+")) {
-				while(!converter.isEmpty() && !converter.peek().equals("(")) {
-					postfix += converter.pop() + " ";
-				}
-				converter.push("+");
-			}
-			else if(token.equals("-")) {
-				while (converter.peek().equals("+") || converter.peek().equals("-")) {
-					postfix= postfix + converter.pop() + " ";
-				}
-				converter.push("/");
-			}
-			else{
-				converter.push(token);
-			}
+      while (in.hasNext()) {
+         String token = in.next();
 
-		}
-		while(!converter.isEmpty()){
-			postfix= postfix+ converter.pop() + " ";
-		}
-		return postfix;
-		
-	}
-	
-	/**
-	 * Calculate the value of the postfix expression.
-	 * @param expression the postfix expression to be calculated.
-	 * @return the value Double.parseDouble((String)of the postfix e)xpression.
-	 */
-	public static double postfixValue (String expression)
-	{
-		double lhs, rhs, answer;
-		//Creates a new stack to hold inputs
-		MyStack calculator = new MyStack();
+         if (token.equals("(")) {
+            converter.push(token);
+         } else if (token.equals(")")) {
+            peekNext = (String) converter.peek();
+            while (!peekNext.equals("(")) {
+               postfix += converter.pop() + " ";
+               peekNext = (String) converter.peek();
+            }
+            converter.pop();
+         }
+         else if (token.equals("*")) {
+            converter.push("*");
+         }
+         else if (token.equals("/")) {
+            converter.push("/");
+         }
+         else if (token.equals("+")) {
+            while (!converter.isEmpty() && !converter.peek().equals("(")) {
+               postfix += converter.pop() + " ";
+            }
+            converter.push("+");
+         } else if (token.equals("-")) {
+            while (!converter.isEmpty() && !converter.peek().equals("(")) {
+               postfix += converter.pop() + " ";
+            }
+            converter.push("-");
+         } else {
+            converter.push(token);
+         }
 
-		//Creates a scanner to process the input expression
-		Scanner in = new Scanner(expression);
-		//
-		while (in.hasNext()){
-			String token = in.next();
-			if(token.matches("[0-9]")){
-				calculator.push(token);
-			}
-			else if (token.equals("^")){
-				rhs= Double.parseDouble((String)calculator.pop());
-				lhs = Double.parseDouble((String)calculator.pop());
-				calculator.push(String.valueOf(Math.pow(lhs,rhs)));
-			}
-			else if (token.equals("*")){
-				rhs= Double.parseDouble((String)calculator.pop());
-				lhs = Double.parseDouble((String)calculator.pop());
-				calculator.push(String.valueOf(lhs*rhs));
-			}
-			else if (token.equals("/")){
-				rhs= Double.parseDouble((String)calculator.pop());
-				lhs = Double.parseDouble((String)calculator.pop());
-				calculator.push(String.valueOf(lhs/rhs));
-			}
-			else if (token.equals("+")){
-				rhs= Double.parseDouble((String)calculator.pop());
-				lhs = Double.parseDouble((String)calculator.pop());
-				calculator.push(String.valueOf(lhs+rhs));
-			}
-			else if (token.equals("-")){
-				rhs= Double.parseDouble((String)calculator.pop());
-				lhs = Double.parseDouble((String)calculator.pop());
-				calculator.push(String.valueOf(lhs-rhs));
-			}
-		}
-		return(Double.parseDouble((String)calculator.pop()));
-	}
+      }
+      while (!converter.isEmpty()) {
+         postfix = postfix + converter.pop() + " ";
+      }
+      return postfix;
+
+   }
+
+   /**
+    * Calculate the value of the postfix expression.
+    *
+    * @param expression the postfix expression to be calculated.
+    * @return the value Double.parseDouble((String)of the postfix e)xpression.
+    */
+   public static double postfixValue(String expression) {
+      double lhs, rhs;
+      
+      //Creates a new Stack
+      MyStack stack = new MyStack();
+      String[] inputs = expression.split("\\s");
+ 
+
+      for (int i = 0; i < inputs.length; i++) {
+         if (inputs[i].equals("*") || inputs[i].equals("/") || inputs[i].equals("+") || inputs[i].equals("-")) {
+            rhs = Double.valueOf((String) stack.pop());
+            lhs = Double.valueOf((String) stack.pop());
+            switch (inputs[i].charAt(0)) {
+               
+               case '*':
+                  stack.push(String.valueOf(lhs * rhs));
+                  break;
+               
+               case '/':
+                  stack.push(String.valueOf(lhs / rhs));
+                  break;
+               
+               case '+':
+                  stack.push(String.valueOf(lhs + rhs));
+                  break;
+               
+               case '-':
+                  stack.push(String.valueOf(lhs - rhs));
+                  break;
+            }
+
+         } else {
+            stack.push(inputs[i]);
+         }
+      }
+
+      return Double.valueOf((String)stack.pop());
+   }
 }
